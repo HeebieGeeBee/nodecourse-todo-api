50c7a01f90c07a886d6cd0ef2d9ca6149b754d83
Geoff Bull <geoffbull29@gmail.com>
2018-10-15 01:13:13 +0200

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,11 @@
 var mongoose = require('mongoose');
 
+let db = {
+    localhost: 'mongodb://localhost:27017/TodoApp',
+    mlab: 'mongodb://Geeheeb:fTwpDKK7CG3Zy4y@ds131983.mlab.com:31983/geoffs-todo'
+}
+
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp'); 
 
 module.exports = {mongoose};
