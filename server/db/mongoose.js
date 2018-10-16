1913fa9fa59a6fe134df683532d696b8406592d8
Geoff Bull <geoffbull29@gmail.com>
2018-10-17 00:39:39 +0200

    Add test databases part 1

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,17 +1,22 @@
 var mongoose = require('mongoose');
 
+
 let db = {
     localhost: 'mongodb://localhost:27017/TodoApp',
     mlab: 'mongodb://Geeheeb:fTwpDKK7CG3Zy4y@ds131983.mlab.com:31983/geoffs-todo'
 }
 
+process.env.MONGODB_URI = 'mongodb://Geeheeb:fTwpDKK7CG3Zy4y@ds131983.mlab.com:31983/geoffs-todo';
+
 mongoose.Promise = global.Promise;
-mongoose.connect(db.mlab || db.localhost, { useNewUrlParser: true }, (err, data) => {
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true }, (err, data) => {
     if(err) {
         console.log('unable to connect to database', err);
         return;
     }
-    //console.log('connected to data', data);
 }); 
 
-module.exports = {mongoose};
+module.exports = {
+    mongoose,
+    db
+};
